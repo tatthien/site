1391d83342ceb4dc2258de0cfd4b9144c3d9fe31
Thien Nguyen <tatthien.contact@gmail.com>
2024-10-29 11:36:32 +0700

    chore: add avatar

--- components/screens/home/index.tsx
+++ components/screens/home/index.tsx
@@ -7,8 +7,13 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-6">
       <Card>
-        <h1 className="font-semibold text-xl">Thien Nguyen (aka. Daniel)</h1>
-        <h2 className="mb-0 font-normal">Sharing my interest in a little of everything that crosses my mind, but mostly in technology.</h2>
+        <div className='flex gap-4 items-center'>
+          <img src="/img/avatar.svg" alt="Thien Nguyen" className='w-32 md:w-44' />
+          <div>
+            <h1 className="font-semibold text-xl">Thien Nguyen (aka. Daniel)</h1>
+            <h2 className="mb-0 font-normal">Sharing my interest in a little of everything that crosses my mind, but mostly in technology.</h2>
+          </div>
+        </div>
       </Card>
       <Card>
         <Link href={'/posts'} prefetch={true} className="flex justify-between">
