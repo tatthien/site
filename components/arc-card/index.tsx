a000b785b8993f884ab4ced3d5e4fe96b6d6cdea
Thien Nguyen <tatthien.contact@gmail.com>
2024-12-27 13:23:04 +0700

    chore: update homepage

--- components/arc-card/index.tsx
+++ components/arc-card/index.tsx
@@ -2,7 +2,7 @@
 
 import { useRef } from 'react'
 
-export function ArcCard() {
+export function ArcCard({ children }: { children: React.ReactNode }) {
   const boundingRef = useRef<DOMRect | null>(null)
 
   return (
@@ -28,22 +28,9 @@ export function ArcCard() {
           ev.currentTarget.style.setProperty('--x', `${xPercentage * 100}%`)
           ev.currentTarget.style.setProperty('--y', `${yPercentage * 100}%`)
         }}
-        className="relative grid w-[260px] grid-rows-[200px_100px_40px] gap-4 rounded-2xl border border-gray-4 bg-[#fff] p-4 text-[#01a977] transition-transform ease-out hover:[transform:rotateX(var(--x-rotation))_rotateY(var(--y-rotation))_scale(1.1)]"
+        className="transition-transform ease-out hover:[transform:rotateX(var(--x-rotation))_rotateY(var(--y-rotation))_scale(1.1)]"
       >
-        <figure className="flex justify-center rounded-sm [background-image:radial-gradient(at_70%_40%,transparent_30%,currentColor_80%)]">
-          <img src="/img/avatar.svg" alt="Avatar" className="h-full w-auto" />
-        </figure>
-        <div>
-          <p className="font-bold text-3xl">Thien Nguyen</p>
-          <p className="text-xl">Software Engineer</p>
-        </div>
-        <footer className="flex items-end">
-          <p className="flex rounded-sm border border-current px-1 py-1 text-[9px] uppercase">
-            thien.dev{' '}
-            <span className="-my-1 mx-1 inline-block w-6 border-current border-r border-l bg-[repeating-linear-gradient(-45deg,currentColor,currentColor_1px,transparent_1px,transparent_2px)]" />{' '}
-            2024
-          </p>
-        </footer>
+        {children}
       </div>
     </div>
   )
--- components/screens/home/index.tsx
+++ components/screens/home/index.tsx
@@ -22,12 +22,29 @@ const projects = [
 export default function Home() {
   return (
     <div className="flex flex-col gap-6">
-      <div className="mb-10 flex flex-col items-center gap-12 md:flex-row">
-        <ArcCard />
+      <div className="mb-10 flex flex-col items-center gap-8 md:flex-row">
+        <ArcCard>
+          <div className='relative grid w-full md:w-[240px] grid-rows-[200px_80px_40px] gap-4 rounded-2xl border border-gray-4 shadow-md bg-[#fff] p-4'>
+            <figure className="flex justify-center rounded-sm">
+              <img src="/img/avatar.webp" alt="Avatar" className="w-full h-auto object-cover rounded-sm" />
+            </figure>
+            <div>
+              <p className="font-bold text-2xl">Daniel Nguyen</p>
+              <p className="text-lg">Software Engineer</p>
+            </div>
+            <footer className="flex items-end">
+              <p className="flex rounded-sm border border-current px-1 py-1 text-[9px] uppercase">
+                thien.dev{' '}
+                <span className="-my-1 mx-1 inline-block w-6 border-current border-r border-l bg-[repeating-linear-gradient(-45deg,currentColor,currentColor_1px,transparent_1px,transparent_2px)]" />{' '}
+                {new Date().getFullYear()}
+              </p>
+            </footer>
+          </div>
+        </ArcCard>
         <div>
-          <p className="mb-4 font-light text-xl">Sharing my interest in a little of everything that crosses my mind, but mostly in technology.</p>
-          <p className="font-light text-xl">Me on other platforms:</p>
-          <nav className="flex flex-col gap-2">
+          <p className="mb-4">Hello, I'm Daniel Nguyen, a Software Engineer specializing in JavaScript, including its ecosystem like React, Next.js, and Node.js. I also work with Go on occasion. This is my little corner of the internet where I share my interest in a little of everything that crosses my mind, but mostly about technology.</p>
+          <p>Connect with me on:</p>
+          <nav className="flex gap-6">
             <a href="https://github.com/tatthien" className="flex items-center gap-2 text-gray-9">
               <Github width={18} />
               GitHub
@@ -44,25 +61,23 @@ export default function Home() {
         </div>
       </div>
       <Card>
+        <h2 className="py-2 text-gray-9 capitalize">Side projects</h2>
+        <div className="flex flex-col gap-2">
+          {projects.map((project) => (
+            <a key={project.name} href={project.url} target='_blank' className='rounded-md px-2 py-1 -mx-2 hover:bg-gray-3 hover:opacity-100'>
+              <h3>
+                {project.name}
+              </h3>
+              <p className="my-0 text-gray-9 text-sm">{project.description}</p>
+            </a>
+          ))}
+        </div>
+      </Card>
+      <Card>
         <Link href={'/posts'} prefetch={true} className="flex justify-between">
           <h2 className="py-2 text-gray-9 capitalize">Writing ⇢</h2>
         </Link>
-        <Posts category="posts" limit={10} />
-      </Card>
-      <Card>
-        <h2 className="py-2 text-gray-9 capitalize">Side projects</h2>
-        <div className="flex flex-col gap-4">
-          {projects.map((project) => (
-            <div key={project.name}>
-              <h3>
-                <a href={project.url} target="_blank" rel="noreferrer">
-                  {project.name}
-                </a>
-              </h3>
-              <p className="my-0 text-gray-9 text-sm">{project.description}</p>
-            </div>
-          ))}
-        </div>
+        <Posts category="posts" />
       </Card>
       <Footer />
     </div>
